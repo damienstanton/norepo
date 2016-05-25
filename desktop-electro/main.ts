c0e9b2f52405e9953f259b0e23f9b7a21216bd0b
Damien Stanton <damien@blinkanalytics.com>
2016-05-25 12:52:38 -0400

    rewrite electron main in typescript

---
+++ desktop-electro/main.ts
@@ -0,0 +1,37 @@
+// Delcare the vanilla js vars as "any" type
+declare var require: any;
+declare var __dirname: any;
+declare var process: any;
+
+const electron = require("electron");
+const {app} = electron;
+const {BrowserWindow} = electron;
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({width: 800, height: 600});
+  win.loadURL(`file://${__dirname}/electro/www/index.html`);
+
+  // DEBUG open the DevTools in case something is awry
+  // win.webContents.openDevTools();
+
+  win.on("closed", () => {
+    win = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+// These two functions ensure good behavior on OSX
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
