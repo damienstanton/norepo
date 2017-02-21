6a2b77271b7e6a7ef1b12263322b035251e5af66
Damien Stanton <damien@blinkanalytics.com>
2017-02-21 17:16:31 -0500

    toy search examples

---
+++ conpat/toysearch.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var (
+	Web   = fakeSearch("web")
+	Image = fakeSearch("Image")
+	Video = fakeSearch("Video")
+)
+
+// Result is a fake result
+type Result string
+
+// Search is our simulated search data structure
+type Search func(query string) Result
+
+func fakeSearch(k string) Search {
+	return func(q string) Result {
+		time.Sleep(SleepVal)
+		return Result(fmt.Sprintf("%s result for %q\n", k, q))
+	}
+}
+
+// Googlev10 does a web search on that site
+func GoogleV10(q string) (res []Result) {
+	res = append(res, Web(q))
+	res = append(res, Image(q))
+	res = append(res, Video(q))
+	return res
+}
+
+// Googlev20 does a better web search on that site
+func GoogleV20(q string) (finalResults []Result) {
+	c := make(chan Result)
+	// concurrently rather than serialized
+	go func() {
+		c <- Web(q)
+	}()
+	go func() {
+		c <- Image(q)
+	}()
+	go func() {
+		c <- Video(q)
+	}()
+
+	for i := 0; i < 3; i++ {
+		res := <-c
+		finalResults = append(finalResults, res)
+	}
+	return finalResults
+}
+
+// Googlev21 does a betterweb search on that site
+func GoogleV21(q string) (finalResults []Result) {
+	c := make(chan Result)
+	go func() {
+		c <- Web(q)
+	}()
+	go func() {
+		c <- Image(q)
+	}()
+	go func() {
+		c <- Video(q)
+	}()
+
+	// now no need to wait for slow servers. Still no locks, condition vars,
+	// or callbacks. This is nice.
+	timeout := time.After(80 * time.Millisecond)
+	for i := 0; i < 3; i++ {
+		select {
+		case res := <-c:
+			finalResults = append(finalResults, res)
+		case <-timeout:
+			fmt.Println("Timed out")
+			return
+		}
+	}
+	return finalResults
+}
